3f6fa7a1e0609eba22fe5a5028a2fb6f42ff696b
Christian <christiandp7@gmail.com>
2020-09-05 18:31:14 -0400

    Add Home Card

--- src/Views/home.js
+++ src/Views/home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import {
   Col,
@@ -6,26 +6,61 @@ import {
   Card,
 } from 'react-bootstrap'
 
-function home() {
+import { getUsuarios } from '../http/index'
+
+const home = () => {
+
+  //const [tot, setTot] = useState([])
+
+  
+
+
   return (
     <div style={{ marginTop: '30px' }}>
       <Row>
         <Col>
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
+          <Card style={{ maxWidth: '18rem' }}>
+            <Card.Body>
+              <Card.Title>Total de Usuarios</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">En base de datos</Card.Subtitle>
+              <Card.Text>
+                <h1></h1>
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </Col>
         <Col>
-        
+          <Card style={{ maxWidth: '18rem' }}>
+            <Card.Body>
+              <Card.Title>Usuarios Restantes</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">En base de datos</Card.Subtitle>
+              <Card.Text>
+                <h1>200</h1>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+        <Card style={{ maxWidth: '18rem' }}>
+            <Card.Body>
+              <Card.Title>Total de Comentarios</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Christian</Card.Subtitle>
+              <Card.Text>
+                <h1>200</h1>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+        <Card style={{ maxWidth: '18rem' }}>
+            <Card.Body>
+              <Card.Title>Total de Comentarios</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Andrea</Card.Subtitle>
+              <Card.Text>
+                <h1>200</h1>
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
       
